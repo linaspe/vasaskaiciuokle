1ebd0d8fe1525c7278eb59a900bc214e04910cc5
Linas <linas@lekiam.lt>
2022-06-13 20:33:53 +0300

    .

--- skaiciuokle.js
+++ skaiciuokle.js
@@ -35,8 +35,8 @@
 
     let val;
 
-        if(item == 3) val = 1 * objektoTipas[3].c;
-        else if(item == 4) val = 12 * objektoTipas[4].c;
+        if(objektoTipas[item].g == 3) val = 1 * objektoTipas[item].c;
+        else if(objektoTipas[item].g == 4) val = 12 * objektoTipas[item].c;
         else val = round(size * objektoTipas[item].c *12, 2 );
 
         return val;
@@ -45,10 +45,10 @@
     function calc_second_part(size, item) {
         let val;
 
-        if(item == 1) val = round(size * objektoTipas[1].v *12, 2) ;
-        else if(item == 2) val = round(size * objektoTipas[2].v * 12, 2) ;
-        else if(item == 3) val = round(32.5 * 0.0850 *12, 2) ;
-        else if(item == 4) val = 1 * objektoTipas[item].v;
+        if(objektoTipas[item].g == 1) val = round(32.5 * 0.0850 *12, 2) ;
+        else if(objektoTipas[item].g== 2) val = round(size * objektoTipas[item].v * 12, 2) ;
+        else if(objektoTipas[item].g== 3) val = round(32.5 * 0.0850 *12, 2) ;
+        else if(objektoTipas[item].g == 4) val = 1 * objektoTipas[item].v;
 
         return val;
     }
